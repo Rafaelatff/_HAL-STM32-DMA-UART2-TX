29538114a658b9451a1f3ac47701a2b268935678
Rafaela Freitas <58916022+Rafaelatff@users.noreply.github.com>
2023-01-26 17:41:36 -0300

    add source files

---
+++ Core/Src/data_stream.c
@@ -0,0 +1,15 @@
+/*
+ * data_stream.c
+ *
+ *  Created on: 26 de jan de 2023
+ *      Author: r.freitas
+ */
+#include <stdint.h>
+
+uint8_t data_stream [] = "This repository was create to follow the content of the course 'ARM Cortex M Microcontroller DMA Programming Demystified'.\
+I am using:\
+NUCLEO-F401RE Board\
+STM32Cube FW_F4 V1.27.1\
+STM32CubeIDE version 1.10.1";
+
+uint32_t data_len = sizeof(data_stream);
